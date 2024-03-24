802f3844566142d04a1e8518cff1eda75b43dff2
Zaasth <wellintonpaypal@gmail.com>
2024-03-23 23:39:35 -0300

    commit

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 GerarCorAleatoriaHexa();
 
+
 function GerarCorAleatoriaHexa() {
     const body = document.querySelector("body")
     const botão = document.querySelector("#botão");
-    const form = document.querySelector("#form");
-    const letras = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const form = document.querySelector("#form");  
     const title = document.querySelector("#title");
-
+    
     form.addEventListener("submit", function(event){
         event.preventDefault();
+        const numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+        let RandomColor = "#"
         
-        let RandomNumber1 = Math.floor(Math.random()*10),RandomNumber2 = Math.floor(Math.random()*10), RandomNumber3 = Math.floor(Math.random()*10), RandomNumber4 = Math.floor(Math.random()*10), RandomNumber5 = Math.floor(Math.random()*10), RandomNumber6 = Math.floor(Math.random()*10);
-    
-        let RandomColor = "#"+letras[RandomNumber1]+letras[RandomNumber2]+letras[RandomNumber3]+letras[RandomNumber4]+letras[RandomNumber5]+letras[RandomNumber6];
+        for(let i = 0; i < 6; i++){
+            RandomColor += numeros[RandomNumberFunction()]
+        }
         body.style.backgroundColor = RandomColor;
         title.innerHTML = "A sua cor é: " + RandomColor; 
+        
+        RandomNumberFunction();
+        function RandomNumberFunction(){
     
-        console.log("#"+letras[RandomNumber1]+letras[RandomNumber2]+letras[RandomNumber3]+letras[RandomNumber4]+letras[RandomNumber5]+letras[RandomNumber6])
+            let numerosSize = (numeros.length)
+            return Math.floor(Math.random()*(numerosSize));
+        }
     
     });
+    
 }
